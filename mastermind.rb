2db31fecf509be82a65d4bfb8a7378b3587a35d2
Maria Simina <maria.simina_groza@yahoo.com>
2023-10-12 13:22:46 +0300

    Choose or guess the code, player and computer

--- mastermind.rb
+++ mastermind.rb
@@ -1,57 +1,37 @@
-class Board
-  attr_reader :pegs
-
+class Player
   def initialize
     @pegs = ["red", "green", "blue", "yellow", "white", "black"]
   end
 
-  def create_code
+  def random_choice
     @code = @pegs.sample(4)
+  end
+
+  def choose_row
+    puts "Choose a row of 4 colors between red, green, blue, yellow, white, black."
+    @colors_row = gets.chomp
+    @colors_row = @colors_row.split(" ")
+    if (@pegs - @colors_row).length > 2
+      puts "Invalid choice"
+      choose_row
+    else
+      @colors_row
+    end
   end
 end
 
 class Game
   def initialize
-    @board = choose_role
-    @code = @board.create_code
-    @new_row = []
-  end
-
-  def choose_role
-    puts "What would you like to do, chose the code or guess the code?"
-    puts "Please chose between: 'chose the code' and 'guess the code'."
-    player = gets.chomp
-    if player == "guess the code"
-      Board.new
-    elsif player == "chose the code"
-      Player.new
-    else
-      puts "Invalid choice"
-      choose_role
-    end
-  end
-
-  def guess_color
-    puts "Choose a color"
-    color = gets.chomp
-    if @board.pegs.none? {|choice| color == choice}
-      guess_color
-    else
-      @new_row << color
-    end
-  end
-
-  def get_row
-    @new_row = []
-    4.times {guess_color}
-    p @new_row
+    @code = nil
+    @guess = nil
+    @tries = 0
   end
 
   def keys
     red_keys = 0
     white_keys = 0
     @code.each_with_index do |choice, index|
-      @new_row.each do |color, position|
+      @guess.each_with_index do |color, position|
         if (choice == color) && (index == position)
           red_keys += 1
         elsif (choice == color) && (index != position)
@@ -63,29 +43,35 @@ class Game
   end
 
   def check_guess
-    if @new_row == @code
+    if @guess == @code
       puts "You won!"
+      @tries = 12
     else
       keys
-      puts "\n" + "Try again!"
-      get_row
-      check_guess
+      @tries += 1
     end
   end
 
   def play
-    p @code
-    get_row
-    check_guess
-  end
-end
-
-class Player < Board
-
-  def create_code
-    puts "Enter code bellow:"
-    @code = gets.chomp
-    @code = @code.split(" ")
+    new_player = Player.new
+    puts "What would you like to do? Please choose between: 'choose the code' and 'guess the code'."
+    player = gets.chomp
+    if player == "guess the code"
+      @code = new_player.random_choice
+      until @tries == 12 do
+        @guess = new_player.choose_row
+        check_guess
+      end
+    elsif player == "choose the code"
+      @code = new_player.choose_row
+      until @tries == 12 do
+        @guess = new_player.random_choice
+        check_guess
+      end
+    else
+      puts "Invalid choice"
+      play
+    end
   end
 end
 
